93ef37a6afb51514c78da96452d1bfcd4d75b903
Reese Hudson <david.reese.hudson@gmail.com>
2020-11-19 21:36:27 -0500

    add axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Header from './Components/Header.js'
+import Registration from './Components/Registration.js'
+import Footer from './Components/Footer.js'
 import './App.css';
 
+import {
+  Button, Modal, ModalHeader, ModalBody,
+  ModalFooter, Input, Label
+} from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Header />
+          <Router>
+            <Button color="danger" onClick={toggle}>Sign In</Button>
+            <Modal isOpen={modal} toggle={toggle}>
+              <ModalHeader toggle={toggle}>Register New User</ModalHeader>
+              <ModalBody>
+                <Label>Name</Label>
+                <Input className='mb-2' />
+                <Label className='mt-4'>Email</Label>
+                <Input className='mb-2' type="email" name="email" id="exampleEmail" placeholder="" />
+                <Label className='mt-4'>Password</Label>
+                <Input className='mb-2' type="password" name="password" id="examplePassword" placeholder="" />
+              </ModalBody>
+              <ModalFooter>
+                <Button type="submit" color="primary" onClick={toggle}>Register</Button>{' '}
+                <Button color="secondary" onClick={toggle}>Cancel</Button>
+              </ModalFooter>
+            </Modal>
+            <Registration />
+
+            {/* <Switch>
+              <Route path="/playerRegistration">
+              </Route>
+              <Route path="/">
+              </Route>
+            </Switch> */}
+          </Router>
+        </div>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+//import  from './Components/'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+  } from 'reactstrap';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Wordmark from '../img/IA3_Wordmark_Black.png'
+
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+
+    
+    return (
+        <div>
+            <header>
+                <Navbar color="danger" light expand="md">
+                    <NavbarBrand href="/"><img src='../img/IA3_Wordmark_Black.png' alt='wordmark'/></NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>About Us</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>Sports</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>Contact</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
